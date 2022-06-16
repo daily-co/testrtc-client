0b5ba83dde4c38ea701906418754bfcb700427f6
Liza <65890040+lazeratops@users.noreply.github.com>
2022-06-16 21:38:12 +0200

    First iteration of TestRTC Daily Prebulit embed

---
+++ README.md
@@ -0,0 +1,16 @@
+# Daily TestRTC client
+
+This repository is a basic embed of Daily Prebuilt, intended to be optimized as needed for TestRTC perf tests.
+
+It takes a `roomURL` query parameter to detect which room to join.
+
+## Running locally
+
+1. `npm i`
+1. `npm run build`
+1. `npm run start`
+
+## Contributing and feedback
+
+Please run `npm run fix` to run eslint and prettier. 
+This will format and auto-fix what it can, outputting any remaining issues you need to handle manually to the console.
---
+++ src/index.ts
@@ -0,0 +1,72 @@
+import './env';
+import DailyIframe from '@daily-co/daily-js';
+
+// These imports ensure relevant assets are bundled
+// with the rest of the build
+import './html/index.html';
+import './css/style.css';
+import './assets/daily.svg';
+import './assets/favicon.ico';
+import './assets/github.png';
+import './assets/new-tab-icon.png';
+
+function getContainer(): HTMLDivElement {
+  return <HTMLDivElement>document.getElementById('container');
+}
+
+function updateJoinedElement(isVisible: boolean) {
+  const inCall = document.getElementById('inCall');
+  const c = 'hidden';
+  isVisible ? inCall.classList.remove(c) : inCall.classList.add(c);
+}
+
+// Join the video call
+function joinCall(roomURL: string) {
+  const container = getContainer();
+
+  // Update the following as required to optimize for perf tests
+  const callFrame = DailyIframe.createFrame(container, {
+    showLeaveButton: true,
+    activeSpeakerMode: false,
+    layoutConfig: {
+      grid: {
+        maxTilesPerPage: 1,
+      },
+    },
+    iframeStyle: {
+      position: 'fixed',
+      width: 'calc(100% - 1rem)',
+      height: 'calc(100% - 5rem)',
+    },
+  });
+
+  // Set up a couple of handlers to make it simpler to detect
+  // when we're in from TestRTC
+  callFrame
+    .on('joined-meeting', () => {
+      updateJoinedElement(true);
+    })
+    .on('left-meeting', () => {
+      updateJoinedElement(false);
+    });
+
+  // Join!
+  callFrame.join({
+    url: roomURL,
+    userName: 'Robot',
+  });
+}
+
+// When the DOM is loaded, init room join
+window.addEventListener('DOMContentLoaded', () => {
+  // Grab room URL from query string
+  const usp = new URLSearchParams(window.location.search);
+  const params = Object.fromEntries(usp.entries());
+
+  if (!params.roomURL) {
+    const c = getContainer();
+    c.innerText = 'room URL not provided';
+    return;
+  }
+  joinCall(params.roomURL);
+});
