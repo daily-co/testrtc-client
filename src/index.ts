86b13b5cd258283ad23801e380f9895b674b9c79
Liza Shulyayeva <65890040+lazeratops@users.noreply.github.com>
2022-08-03 22:16:04 +0200

    allow customizable video tile size

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,36 @@ import './assets/new-tab-icon.png';
 import { createRoom } from './create';
 import { joinCall } from './join';
 
+// setTileSize will dictate dimensions of
+// the participants' video tiles.
+function setTileSize(sizeData: string) {
+  const root = document.documentElement;
+  type Size = {
+    width: string;
+    height: string;
+  };
+
+  const tileSize = <Size>JSON.parse(sizeData);
+  if (!tileSize.width || !tileSize.height) {
+    throw new Error('given tile size must contain width and height');
+  }
+
+  root.style.setProperty('--video-width', tileSize.width);
+  root.style.setProperty('--video-height', tileSize.height);
+}
+
 // When the DOM is loaded, init room join
 window.addEventListener('DOMContentLoaded', () => {
   const usp = new URLSearchParams(window.location.search);
   const params = Object.fromEntries(usp.entries());
+
+  if (params.tileSize) {
+    try {
+      setTileSize(params.tileSize);
+    } catch (e) {
+      console.error('failed to set tile size:', e);
+    }
+  }
 
   // If room URL is provided, just join the call
   if (params.roomURL) {
